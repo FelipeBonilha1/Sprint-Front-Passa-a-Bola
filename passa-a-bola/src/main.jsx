e415b36f6268e37ee53cdf27cfcd4be0e3451ca4
Felipe Bonilha <felpsbonilha@gmail.com>
2025-10-02 16:49:17 -0300

    ajustes no header

--- passa-a-bola/src/main.jsx
+++ passa-a-bola/src/main.jsx
@@ -8,7 +8,7 @@ import Shell from "./ui/Shell.jsx";
 import AppLoader from "./components/AppLoader.jsx";
 import BootSplash from "./components/BootSplash.jsx";
 
-// --- suas páginas lazy (como já deixei antes) ---
+
 const Home      = lazy(() => import("./pages/home.jsx"));
 const Login     = lazy(() => import("./pages/Login.jsx"));
 const Profile   = lazy(() => import("./pages/Profile.jsx"));
